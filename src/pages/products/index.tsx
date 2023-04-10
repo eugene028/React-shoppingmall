55a30a373f5eb5dcfb7e366d9f36cd32646fa9ea
Eugene Kim <eugene02876@gmail.com>
2023-04-10 20:58:34 +0900

    [feat]: 쇼핑몰 리스트 apI 통신확인

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,31 @@
-const ProductPage = () => <div>상품목록</div>
+import { useQuery } from 'react-query';
+import { fetcher, QueryKeys } from '../../queryClient';
+import { Product } from '../../types';
+
+const ProductPage = () => {
+    const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () => fetcher({
+        method: 'GET',
+        path: '/products'
+    }))
+    console.log(data);
+    return (
+        <div>
+            <ul>
+                {data?.map(product => (
+                    <ProductItem {...product} key={product.id}/>
+                ))}
+            </ul>
+        </div>
+    )
+}
+/*
+category
+description
+id
+image
+price
+rating
+title
+*/
 
 export default ProductPage
