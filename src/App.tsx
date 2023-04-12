d198ca9d7008db72ea6626fb138815e251a5c1fb
Eugene Kim <eugene02876@gmail.com>
2023-04-13 00:42:00 +0900

    [feat] : nav바 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { routes } from './routes';
 import { getClient } from '@libs/apis/products/queryClientApi'
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Gnb from '@components/gnb';
 
 const App = () => {
     const elem = useRoutes(routes);
     const queryClient = getClient();
     return (
         <QueryClientProvider client = {queryClient}>
+            <Gnb/>
             {elem}
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
